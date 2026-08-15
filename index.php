5bfea7fe8d6b2fec5fe03c4078f100c0cc1d367c
fatimayehaa-hue <inf875.fatima.yehaa@student.uobabylon.edu.iq>
2026-08-15 16:06:14 +0300

    start

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>fatima shope</title>
+</head>
+<body>
+<center>
+    <div class="main">
+        <form action="insert.php" method="post">
+<h2></h2>
+        </form>
+    </div>
+    <div id="main"></div>
+</center>
+
+    
+</body>
+</html>
