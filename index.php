48b1d4e463eb929ef79966bbc074a56bccf0504e
fatimayehaa-hue <inf875.fatima.yehaa@student.uobabylon.edu.iq>
2026-08-15 20:08:20 +0300

    update

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 <html lang="en">
 
 <head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200..1000&family=Hedvig+Letters+Serif:opsz@12..24&family=Playwrite+DE+SAS+Guides&display=swap" rel="stylesheet">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>fatima shope</title>
@@ -15,11 +18,11 @@
                 <h2>fatima shope</h2>
                 <img src="logo.png" alt="logo" width="250">
                 <br>
-                <input type="text" name="name" >
+                <input type="text" name="name">
                 <br>
                 <input type="text" name="price">
                 <br>
-                <input type="file" id="file" name ="img" style="display:none;">
+                <input type="file" id="file" name="img" style="display:none;">
                 <label for="file">uplod img</label>
                 <button name="uplod">uplod item</button>
                 <br>
