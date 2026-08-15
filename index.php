3c4dc479f787add552da6409c9e5d87c23a07af9
fatimayehaa-hue <inf875.fatima.yehaa@student.uobabylon.edu.iq>
2026-08-15 18:21:37 +0300

    update

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>fatima shope</title>
+    <link rel="stylesheet" href="index.css">
 </head>
 
 <body>
@@ -26,6 +27,7 @@
             </form>
         </div>
         <div id="main"></div>
+        <p>developer dy fatima</p>
     </center>
 
 
