52e3b3b371b5c76bf768a15a2a5f0d1c60ee4e77
fatimayehaa-hue <inf875.fatima.yehaa@student.uobabylon.edu.iq>
2026-08-15 18:12:50 +0300

    update

--- index.php
+++ index.php
@@ -1,20 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>fatima shope</title>
 </head>
-<body>
-<center>
-    <div class="main">
-        <form action="insert.php" method="post">
-<h2></h2>
-        </form>
-    </div>
-    <div id="main"></div>
-</center>
 
-    
+<body>
+    <center>
+        <div class="main">
+            <form action="insert.php" method="post">
+                <h2>fatima shope</h2>
+                <img src="logo.png" alt="logo" width="250">
+                <br>
+                <input type="text" name="name" >
+                <br>
+                <input type="text" name="price">
+                <br>
+                <input type="file" id="file" name ="img" style="display:none;">
+                <label for="file">uplod img</label>
+                <button name="uplod">uplod item</button>
+                <br>
+                <a href="product.php">show all the products</a>
+            </form>
+        </div>
+        <div id="main"></div>
+    </center>
+
+
 </body>
+
 </html>
